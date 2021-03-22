3e09390d872c77237995d6c9d234f31cd8e8489a
Dgonz <Dgonz@LTOP-JARVIS>
2021-03-22 14:58:09 -0400

    Lab 1 Classes Created.

---
+++ src/DannyGonzalezL1/Power.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package DannyGonzalezL1;
+
+import edu.fiu.sysdesign.SelfCheckCapable;
+
+/**
+ * @author Dgonz
+ *
+ */
+public class Power implements SelfCheckCapable {
+	String battery;
+	
+	/**
+	 * Provides power to electronic components
+	*/
+	void Power () {
+		
+	}
+	
+	/**
+	 * Recharges battery
+	 * @return 
+	 * @return
+	 */
+	void Recharge () {
+		
+	}
+	
+	@Override
+	public String getComponentName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean selfCheck() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean runSelfCheck() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/DannyGonzalezL1/Signal.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package DannyGonzalezL1;
+
+import edu.fiu.sysdesign.SelfCheckCapable;
+
+/**
+ * @author Dgonz
+ *
+ */
+public class Signal implements SelfCheckCapable {
+
+	@Override
+	public String getComponentName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean selfCheck() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean runSelfCheck() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
